1405baaf59298085a250db9b8948a6a35431aef3
AyaAlaa010 <AyaAlaa010920@gmail.com>
2021-05-07 17:19:53 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/StoriesI.java
@@ -0,0 +1,8 @@
+package com.example.myapplication1;
+
+import android.view.View;
+
+public interface StoriesI {
+    public void onStoryClick(story mystory);
+    public void onMoreClick(story mystory, View v);
+}
